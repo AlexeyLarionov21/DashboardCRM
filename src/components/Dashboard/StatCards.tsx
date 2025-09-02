aa5e27eacc71cb88babf0b6b5c29f86d9e1461a5
Alexey Larionov <alexeylarionov21@gmail.com>
2025-09-02 23:47:28 +1000

    Upd grid block

--- src/components/Dashboard/StatCards.tsx
+++ src/components/Dashboard/StatCards.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiTrendingUp, FiTrendingDown } from "react-icons/fi";
 
 export const StatCards = () => {
   return (
@@ -42,6 +43,23 @@ const Card = ({
   period: string;
 }) => {
   return (
-    <div className="rounded border border-stone-300 p-4 bg-white col-span-4"></div>
+    <div className="rounded-xl border border-stone-300 p-4 bg-white col-span-4">
+      <div className="flex mb-4 items-start justify-between">
+        <div>
+          <h3 className="text-stone-500 mb-2 text-sm ">{title}</h3>
+          <p className="text-3xl font-semibold">{value}</p>
+        </div>
+        <span
+          className={`text-xs flex items-center gap-1 font-medium px-2 py-1 rounded ${
+            trend === "up"
+              ? "bg-green-100 text-green-700"
+              : "bg-red-100 text-red-700"
+          }`}
+        >
+          {trend === "up" ? <FiTrendingUp /> : <FiTrendingDown />} {pillText}
+        </span>
+      </div>
+      <p className="text-xs text-stone-500">{period}</p>
+    </div>
   );
 };
